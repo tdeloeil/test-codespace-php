beb7333133b6be710c4d56a5bd2f2fb1bdaedfb9
Flender <96743757+FlenderGit@users.noreply.github.com>
2026-01-22 09:44:37 +0100

    tests

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
 
+use Monolog\Logger;
+use OpenTelemetry\API\Globals;
+use OpenTelemetry\Contrib\Logs\Monolog\Handler;
+use Psr\Log\LogLevel;
+
 $host = getenv('DB_HOST');
 $port = getenv('DB_PORT');
 $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
+
+$loggerProvider = Globals::loggerProvider();
+$handler = new Handler($loggerProvider, LogLevel::INFO);
+
+$logger = new Logger('otel-logger', [$handler]);
 
 $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
 
@@ -15,3 +25,5 @@ $stmt = $pdo->query("SELECT * FROM users");
 $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 var_dump($users);
+
+$logger->info('Fetched users from database', ['user_count' => count($users)]);
